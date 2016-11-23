aebe7137610ea690212001339483f12b2fbc2354
gauchoaleman <gauchoaleman@yahoo.com>
2016-11-23 18:25:37 -0300

    Prueba commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Stefan on 23/11/2016.
+ */
+public class Main {
+
+        public static void main(String[] args) {
+            System.out.println("***********************************************");
+            System.out.println("*  Arranca Aplicacion - vamos por el parcial! *");
+            System.out.println("***********************************************");
+    }
+}
